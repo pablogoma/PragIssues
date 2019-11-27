25d6dce99be09c6b4edf6c796dc0253fafddc589
pablogoma <uo258841@uniovi.com>
2019-11-27 21:53:00 +0200

    prueba lab2

---
+++ prueba2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	
+	echo > RevisionNumber.cs static class Git{ public static int
+	RevisionNnumber = git >> RevisionNumber.cs rev-list --count HEAD
+	echo >> RevisionNumber.cs;
+}
+
+	printf("Hello, World!\n");
+	return 0;
+}
